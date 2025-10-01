cc331d18b2c9f4184e69e4f56135789ddbc68191
Isaac To <isaac.chun.to@gmail.com>
2025-09-30 23:05:41 -0700

    feat: provide vendor-specific info to config dandi-schema used by production instance

--- terraform/api.tf
+++ terraform/api.tf
@@ -41,6 +41,10 @@ module "api_heroku" {
     DJANGO_DANDI_DOI_API_USER        = "JHU.NXHEVY" # TODO: Replace with non-test user
     DJANGO_DANDI_DOI_API_PREFIX      = "10.82754" # TODO: Replace with non-test prefix
     DJANGO_DANDI_DOI_PUBLISH         = "true"
+    DJANGO_DANDI_INSTANCE_NAME       = "EMBER-DANDI"
+    DJANGO_DANDI_INSTANCE_IDENTIFIER = "RRID:SCR_026700"
+    DJANGO_DANDI_LICENSES            = jsonencode(["spdx:CC0-1.0", "spdx:CC-BY-4.0", "spdx:CC-BY-NC-SA-4.0"])
+
 
     # These may be removed in the future
     DJANGO_DANDI_DANDISETS_BUCKET_NAME   = module.sponsored_dandiset_bucket.bucket_name
